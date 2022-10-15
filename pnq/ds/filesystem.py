1615a78b78433d7b7ac3b59ae8f1cb935d86d8f6
sasano8 <sasano8@LAPTOP-PCMR5DJL>
2022-10-15 09:43:54 +0900

    update filesystem

--- pnq/ds/filesystem.py
+++ pnq/ds/filesystem.py
@@ -3,32 +3,37 @@ from os.path import isfile
 from pathlib import Path
 
 from pnq import query
+import platform
+
+V_3_10 = int(platform.python_version_tuple()[1]) >= 10
+
+
+# TODO: drop from 3.10
+def _build_args(**kwargs):
+    if V_3_10:
+        return kwargs
+    else:
+        del kwargs["root_dir"]
+        del kwargs["dir_fd"]
+        return kwargs
 
 
 def ls(pathname="*", *, root_dir=None, dir_fd=None, recursive=False):
-    return query(
-        Path(x)
-        for x in iglob(
-            pathname=pathname, root_dir=root_dir, dir_fd=dir_fd, recursive=recursive
-        )
+    kwargs = _build_args(
+        pathname=pathname, root_dir=root_dir, dir_fd=dir_fd, recursive=recursive
     )
+    return query(Path(x) for x in iglob(**kwargs))
 
 
 def files(pathname="*", *, root_dir=None, dir_fd=None, recursive=False):
-    return query(
-        Path(x)
-        for x in iglob(
-            pathname=pathname, root_dir=root_dir, dir_fd=dir_fd, recursive=recursive
-        )
-        if isfile(x)
+    kwargs = _build_args(
+        pathname=pathname, root_dir=root_dir, dir_fd=dir_fd, recursive=recursive
     )
+    return query(Path(x) for x in iglob(**kwargs) if isfile(x))
 
 
 def dirs(pathname="*", *, root_dir=None, dir_fd=None, recursive=False):
-    return query(
-        Path(x)
-        for x in iglob(
-            pathname=pathname, root_dir=root_dir, dir_fd=dir_fd, recursive=recursive
-        )
-        if not isfile(x)
+    kwargs = _build_args(
+        pathname=pathname, root_dir=root_dir, dir_fd=dir_fd, recursive=recursive
     )
+    return query(Path(x) for x in iglob(**kwargs) if not isfile(x))
